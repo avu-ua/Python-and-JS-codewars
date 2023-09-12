ce5d3ef95199ef9912e42b9c96811e23a1232ae8
Olesia Barannik <OlesyaBarannik23@gmail.com>
2023-09-12 20:20:47 +0300

    seve some chenges

--- Python/230912-task05.py
+++ Python/230912-task05.py
@@ -11,20 +11,20 @@
 # All letters will be lowercase and all inputs will be valid.
 
 # ------------------------ Olesia ------------------------
-def high(x):
-    list_sum_of_words = {}
-    input_words = x.split()
-    my_alphabet = "abcdefghijklmnopqrstuvwxyz"
-    my_alphabet_dict = {}
-    for i in my_alphabet:
-        my_alphabet_dict[i] = my_alphabet.index(i)+1
-    for i in input_words:
-        result = 0
-        for j in i:
-            if j in my_alphabet_dict:
-                result += my_alphabet_dict[j]
-        list_sum_of_words[i] = result
-        max_value = max(list_sum_of_words, key=list_sum_of_words.get)
-    return max_value
-
-print(high('up taxi aaaaaaaaaaaaa'))
+# def high(x):
+#     list_sum_of_words = {}
+#     input_words = x.split()
+#     my_alphabet = "abcdefghijklmnopqrstuvwxyz"
+#     my_alphabet_dict = {}
+#     for i in my_alphabet:
+#         my_alphabet_dict[i] = my_alphabet.index(i)+1
+#     for i in input_words:
+#         result = 0
+#         for j in i:
+#             if j in my_alphabet_dict:
+#                 result += my_alphabet_dict[j]
+#         list_sum_of_words[i] = result
+#         max_value = max(list_sum_of_words, key=list_sum_of_words.get)
+#     return max_value
+#
+# print(high('up taxi aaaaaaaaaaaaa'))
