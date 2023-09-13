43dbd512d3c011c0328deb00c8e863d5c040837c
Mariia Novytska <mnovickaa@gmail.com>
2023-09-13 18:17:42 +0300

    +1 task for tommorow

--- Python/230914-task07.py
+++ Python/230914-task07.py
@@ -1,2 +1,6 @@
 # Timer Decorator
 # https://www.codewars.com/kata/56f84d093b164c2e490013cb
+
+
+# Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!
+# https://www.codewars.com/kata/5626b561280a42ecc50000d1/train/python
