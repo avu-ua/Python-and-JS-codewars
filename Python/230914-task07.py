5d16d642f48a29c3f97f6371d9af387678da9f62
Olesia Barannik <OlesyaBarannik23@gmail.com>
2023-09-14 12:17:14 +0300

    added task 6.2 Eureka

--- Python/230914-task07.py
+++ Python/230914-task07.py
@@ -4,3 +4,14 @@
 
 # Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!
 # https://www.codewars.com/kata/5626b561280a42ecc50000d1/train/python
+
+# ------------------------ Olesia ------------------------
+def sum_dig_pow(a, b):
+    my_list = []
+    for i in range(a, b+1):
+        sum_numbers = 0
+        for j, q in enumerate(str(i)):
+            sum_numbers += int(q)**(j+1)
+        if sum_numbers == i:
+            my_list.append(sum_numbers)
+    return my_list
