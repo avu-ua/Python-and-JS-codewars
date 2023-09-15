ada6e46f89dc872d4d1e393d3314a07c39c71899
Olesia Barannik <OlesyaBarannik23@gmail.com>
2023-09-15 11:47:37 +0300

    added solution task8

--- Python/230915-task08.py
+++ Python/230915-task08.py
@@ -1,2 +1,24 @@
 #Make a LIFO stack to reverse a string
 #https://www.codewars.com/kata/5682e72eb7354b2f39000021/train/python
+
+# ------------------------ Olesia ------------------------
+class SiegeState:
+    def __init__(self):
+        self.can_move = False
+        self.damage = 20
+class TankState:
+    def __init__(self):
+        self.can_move = True
+        self.damage = 5
+class Tank:
+    def __init__(self):
+        self.state = TankState()
+
+    def set_state(self, state):
+        self.state = state
+
+    def can_move(self):
+        return self.state.can_move
+
+    def damage(self):
+        return self.state.damage
