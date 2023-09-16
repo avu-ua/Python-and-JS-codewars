0a1d2aea1c01e44007be77a6b2503e6c9a8ec7d8
Mariia Novytska <mnovickaa@gmail.com>
2023-09-16 16:43:11 +0300

    Added task 8 solution and task 9 for today

--- Python/230915-task08.py
+++ Python/230915-task08.py
@@ -19,3 +19,25 @@ class Tank:
         return self.state.can_move
     def damage(self):
         return self.state.damage
+    
+
+# ------------------------ Mariia ------------------------
+class SiegeState:
+    def __init__(self):
+        self.can_move = False
+        self.damage = 20
+
+class TankState:
+    def __init__(self):
+        self.can_move = True
+        self.damage = 5
+
+class Tank:
+    def __init__(self):
+        self.state = TankState()
+
+    def can_move(self):
+        return self.state.can_move
+  
+    def damage(self):
+        return self.state.damage
---
+++ Python/230916-task09.py
@@ -0,0 +1,3 @@
+#WeIrD StRiNg CaSe
+#https://www.codewars.com/kata/52b757663a95b11b3d00062d/train/python
+
