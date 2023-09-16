19ebcb71bc44d9c30ba99f074d71d4eda1a37351
Olesia Barannik <OlesyaBarannik23@gmail.com>
2023-09-16 17:05:41 +0300

    added task2 in 230916

--- Python/230915-task08.py
+++ Python/230915-task08.py
@@ -6,17 +6,22 @@ class SiegeState:
     def __init__(self):
         self.can_move = False
         self.damage = 20
+
 class TankState:
     def __init__(self):
         self.can_move = True
         self.damage = 5
+
 class Tank:
     def __init__(self):
         self.state = TankState()
+
     def set_state(self, state):
         self.state = state
+
     def can_move(self):
         return self.state.can_move
+
     def damage(self):
         return self.state.damage
     
--- Python/230916-task09.py
+++ Python/230916-task09.py
@@ -1,3 +1,7 @@
 #WeIrD StRiNg CaSe
 #https://www.codewars.com/kata/52b757663a95b11b3d00062d/train/python
 
+
+#Build a pile of Cubes
+#https://www.codewars.com/kata/5592e3bd57b64d00f3000047/train/python
+
