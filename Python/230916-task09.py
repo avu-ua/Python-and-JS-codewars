7231bbd526b791864dc7230780d340a1298c5ae1
Mariia Novytska <mnovickaa@gmail.com>
2023-09-18 20:16:25 +0300

    aDDED TASK 9

--- Python/230916-task09.py
+++ Python/230916-task09.py
@@ -2,6 +2,19 @@
 #https://www.codewars.com/kata/52b757663a95b11b3d00062d/train/python
 
 
+# ------------------------ Mariia ------------------------
+def to_weird_case(words):
+    words_list = words.split()
+    weird_str = []
+    for x in words_list:
+        word_iter = list(x)
+        for y in range(0, len(word_iter)):
+            if y%2 == 0 or y == 0:
+                word_iter[y] = word_iter[y].upper()
+            else:
+                word_iter[y] = word_iter[y].lower()
+        weird_str.append("".join(word_iter))
+    return " ".join(weird_str)
 #Build a pile of Cubes
 #https://www.codewars.com/kata/5592e3bd57b64d00f3000047/train/python
 
