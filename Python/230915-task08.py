4223f209cab084eef6e0986041f9a8cdc16ffb20
avu-ua <avuua16@gmail.com>
2023-09-19 12:26:48 +0200

    Task08 Python solution (Slava)

--- Python/230915-task08.py
+++ Python/230915-task08.py
@@ -46,3 +46,25 @@ class Tank:
   
     def damage(self):
         return self.state.damage
+
+
+# ---------------- Slava ---------------------------
+class SiegeState:
+    def __init__(self):
+        self.can_move = False
+        self.damage = 20
+
+class TankState:
+    def __init__(self):
+        self.can_move = True
+        self.damage = 5
+
+class Tank:
+    def __init__(self):
+        self.state = TankState()
+
+    def can_move(self):
+        return self.state.can_move
+
+    def damage(self):
+        return self.state.damage
