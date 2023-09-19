2062bbe069e9e0b16bd050374577babf6bc6422a
avu-ua <avuua16@gmail.com>
2023-09-19 15:49:15 +0200

    Task10 Python solution (Slava)

--- Python/230919-task10.py
+++ Python/230919-task10.py
@@ -19,3 +19,13 @@ def increment_string(strng):
         else:
             new_str = strng[0:(len(strng) - len(num_str))] +"0"*(len(strng) - len(strng[0:(len(strng) - len(num_str))]) - len(num)) + num
     return new_str
+
+
+# ------------------ Slava ----------------------------
+def increment_string(strng):
+    for i in range(len(strng)):
+        if strng[i:].isnumeric():
+            dig_fragment = strng[i:]
+            digit = int(dig_fragment)
+            return strng[0 : i] + strng[i : i + len(dig_fragment) - len(str(digit + 1))] + str(digit + 1)
+    return strng + '1'
