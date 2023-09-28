e040a2682a7972e216985a518b91c00e8dabb434
Mariia Novytska <mnovickaa@gmail.com>
2023-09-28 19:38:32 +0300

    Added all tasks to task16 file

--- Python/230927 - task16.py
+++ Python/230927 - task16.py
@@ -18,6 +18,15 @@ def two_sum(numbers, target):
                 return (i,j)
     return None
 
+# ------------------------ Mariia ------------------------
+def two_sum(numbers, target):
+    for x in range(0, len(numbers)):
+        for y in range(1, len(numbers)):
+            if x == y:
+                continue
+            if numbers[x] + numbers[y] == target:
+                return (x, y)
+
 #Loneliest character
 #https://www.codewars.com/kata/5f885fa9f130ea00207c7dc8
 
@@ -51,3 +60,49 @@ def loneliest(strng):
             spaces.append(left_count + right_count)
     largest = max(spaces)
     return [strng[x] for x, y in enumerate(spaces) if y == largest]
+
+# ------------------------ Mariia ------------------------
+import re
+def loneliest(strn):
+    strn.lstrip()
+    strn.rstrip()
+    letters = [x for x in strn if x.isalpha()]
+    sp = 0
+    char_alone = ""
+    lst_alone = [] 
+    for alpha in letters:
+        i = letters.index(alpha)
+        if i == 0:
+            spases = len(re.findall(f"{alpha}\s*", strn)[0]) - 1
+            if sp < spases:
+                sp = spases
+                char_alone = alpha
+        elif i == len(letters) - 1:
+            spases = len(re.findall(f"\s*{alpha}", strn)[0]) - 1
+            if sp < spases:
+                sp = spases
+                char_alone = alpha
+        else:
+            spases = len(re.findall(f"\s*{alpha}", strn)[0]) + len(re.findall(f"{alpha}\s*", strn)[0]) - 2
+            if sp < spases:
+                sp = spases
+                char_alone = alpha
+                
+    for alpha in letters:
+        i = letters.index(alpha)
+        if i == 0:
+            spases = len(re.findall(f"{alpha}\s*", strn)[0]) - 1
+            if sp == spases:
+                lst_alone.append(alpha)
+        elif i == len(letters) - 1:
+            spases = len(re.findall(f"\s*{alpha}", strn)[0]) - 1
+            if sp == spases:
+                lst_alone.append(alpha)
+        else:
+            spases = len(re.findall(f"\s*{alpha}", strn)[0]) + len(re.findall(f"{alpha}\s*", strn)[0]) - 2
+            if sp == spases:
+                sp = spases
+                lst_alone.append(alpha)
+    return list(tuple(lst_alone))
+        
+    
