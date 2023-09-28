699a07ab33bba18f50a9afd23d68e8f59695e3f1
avu-ua <avuua16@gmail.com>
2023-09-28 12:40:14 +0200

    Task16.1 solution (Slava)

--- Python/230927 - task16.py
+++ Python/230927 - task16.py
@@ -1,5 +1,12 @@
 #Two Sum
 # https://www.codewars.com/kata/52c31f8e6605bcc646000082/train/python
 
+# ------------- Slava ----------------------
+def two_sum(numbers, target):
+    for i in range(len(numbers)):
+        for j in range(len(numbers)):
+            if i != j and numbers[i] + numbers[j] == target:
+                return (i, j)
+
 #Loneliest character
 #https://www.codewars.com/kata/5f885fa9f130ea00207c7dc8
