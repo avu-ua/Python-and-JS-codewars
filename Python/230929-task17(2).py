f633f398dfd15dd1dcea855c1ff037282cf4b7b4
avu-ua <avuua16@gmail.com>
2023-09-29 13:23:03 +0200

    Second part for Task17 in Python added

---
+++ Python/230929-task17(2).py
@@ -0,0 +1,43 @@
+# https://www.codewars.com/kata/64c743cb0a2a00002856ff73
+
+# DESCRIPTION:
+# Given a 2D array of some suspended blocks (represented as hastags),
+# return another 2D array which shows the end result once gravity is switched on.
+
+# Examples
+# switch_gravity([
+#   ["-", "#", "#", "-"],
+#   ["-", "-", "-", "-"],
+#   ["-", "-", "-", "-"],
+#   ["-", "-", "-", "-"]
+# ]) ➞ [
+#   ["-", "-", "-", "-"],
+#   ["-", "-", "-", "-"],
+#   ["-", "-", "-", "-"],
+#   ["-", "#", "#", "-"]
+# ]
+
+# switch_gravity([
+#   ["-", "#", "#", "-"],
+#   ["-", "-", "#", "-"],
+#   ["-", "-", "-", "-"],
+# ]) ➞ [
+#   ["-", "-", "-", "-"],
+#   ["-", "-", "#", "-"],
+#   ["-", "#", "#", "-"]
+# ]
+
+# switch_gravity([
+#   ["-", "#", "#", "#", "#", "-"],
+#   ["#", "-", "-", "#", "#", "-"],
+#   ["-", "#", "-", "-", "-", "-"],
+#   ["-", "-", "-", "-", "-", "-"]
+# ]) ➞ [
+#   ["-", "-", "-", "-", "-", "-"],
+#   ["-", "-", "-", "-", "-", "-"],
+#   ["-", "#", "-", "#", "#", "-"],
+#   ["#", "#", "#", "#", "#", "-"]
+# ]
+# Notes
+# Each block falls individually, meaning there are no rigid objects.
+# Think about it like falling sand in Minecraft as opposed to the rigid blocks in Tetris.
