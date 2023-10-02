3d3e8512f11780158ce1bfee85730691397631de
Mariia Novytska <mnovickaa@gmail.com>
2023-10-02 18:05:47 +0300

    Added tasks for tommorow

---
+++ Python/231002-task19(1).py
@@ -0,0 +1,2 @@
+# Find The Parity Outlier
+# https://www.codewars.com/kata/5526fc09a1bbd946250002dc
---
+++ Python/231002-task19(2).py
@@ -0,0 +1,2 @@
+# Find All the Possible Numbers Multiple of 3 with the Digits of a Positive Integer.
+# https://www.codewars.com/kata/5828b9455421a4a4e8000007
