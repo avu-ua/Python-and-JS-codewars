456e7b40f6bb7389f5c7ccea7138cb911925b5ed
avu-ua <avuua16@gmail.com>
2023-10-02 10:41:32 +0200

    Task18(1) Python - solution

--- Python/231001-task18(1).py
+++ Python/231001-task18(1).py
@@ -1,3 +1,13 @@
 
 #Multiplication table
 #https://www.codewars.com/kata/534d2f5b5371ecf8d2000a08
+
+# ----------------- Slava -----------------------
+def multiplication_table(size):
+    res = []
+    first = [x + 1 for x in range(size)]
+    res.append(first)
+    for r in range(1, size):
+        subsequent = [r + 1 if not c else (r + 1) * first[c] for c in range(size)]
+        res.append(subsequent)
+    return res
