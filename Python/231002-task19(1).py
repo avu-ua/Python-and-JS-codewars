2d0d78bae5f5f32013bc59d139cdd9d79f2c4fa1
Olesia Barannik <OlesyaBarannik23@gmail.com>
2023-10-03 11:31:44 +0300

    added solution task19.1

--- Python/231002-task19(1).py
+++ Python/231002-task19(1).py
@@ -1,2 +1,15 @@
 # Find The Parity Outlier
 # https://www.codewars.com/kata/5526fc09a1bbd946250002dc
+
+# ------------------------ Olesia ------------------------
+def find_outlier(integers):
+    odd_list = []
+    even_list = []
+    for i in integers:
+        if i % 2 == 1:
+            odd_list.append(i)
+        else:
+            even_list.append(i)
+    if len(odd_list) < len(even_list):
+        return odd_list[0]
+    return even_list[0]
