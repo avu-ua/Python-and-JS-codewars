29d18210b7d795244de09d6f31c634f6fc670ed3
Olesia Barannik <OlesyaBarannik23@gmail.com>
2023-10-04 21:38:55 +0300

    added solution task20

--- Python/231004-task20.py
+++ Python/231004-task20.py
@@ -1,2 +1,22 @@
 # Millipede of words
 # https://www.codewars.com/kata/6344701cd748a12b99c0dbc4
+
+# ------------------------ Olesia ------------------------
+
+from itertools import permutations
+
+def solution(arr):
+    n = len(arr)
+    result = list(permutations(range(n)))
+    plural = [list(permutation) for permutation in result]
+    for a in plural:
+        f = 1
+        for i in range(1,len(a)):
+            prev_w = arr[a[i-1]]
+            curr_w = arr[a[i]]
+            if prev_w[-1] != curr_w[0]:
+                f = 0
+                break
+        if f == 1:
+            return True
+    return False
