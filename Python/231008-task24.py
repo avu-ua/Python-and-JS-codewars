428bb7bc21ff7a5535443b239ab82e57218171b2
Vyacheslav Anishchenko <avuua16@gmail.com>
2023-10-07 19:23:18 +0200

    Task24 solution

--- Python/231008-task24.py
+++ Python/231008-task24.py
@@ -1,2 +1,8 @@
 #Wave Sorting
 #https://www.codewars.com/kata/596f28fd9be8ebe6ec0000c1/train/python
+
+# ----------------- Slava --------------------------
+def wave_sort(a):
+    a.sort(reverse=True)
+    for i in range(2, len(a)):
+        if not i % 2: a[i - 1], a[i] = a[i], a[i - 1]
