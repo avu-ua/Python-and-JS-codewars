55d332cc4586ea844498068757bc6fedf773b359
Mariia Novytska <mnovickaa@gmail.com>
2023-10-09 16:34:54 +0300

    Added task 24

--- Python/231008-task24.py
+++ Python/231008-task24.py
@@ -6,3 +6,9 @@ def wave_sort(a):
     a.sort(reverse=True)
     for i in range(2, len(a)):
         if not i % 2: a[i - 1], a[i] = a[i], a[i - 1]
+
+# ------------------------ Mariia ------------------------
+def wave_sort(a):
+    a.sort()
+    for x in range (0, len(a) - 1, 2):
+        a[x], a[x+1] = a[x+1], a[x]
