4401b70b261a2e23750ffbc899d3336557b8cb2b
Olesia Barannik <OlesyaBarannik23@gmail.com>
2023-10-11 14:11:43 +0300

    added solution task26

--- Python/231010-task26.py
+++ Python/231010-task26.py
@@ -1,2 +1,16 @@
 # Shortest steps to a number
 # https://www.codewars.com/kata/5cd4aec6abc7260028dcd942
+
+# ------------------------ Olesia ------------------------
+def shortest_steps_to_num(num):
+    if num == 1:
+        return 0
+    counter = 0
+    n = 2
+    while num > 1:
+        if num % 2 == 0:
+            num = num / 2
+        else:
+            num -= 1
+        counter += 1
+    return counter
