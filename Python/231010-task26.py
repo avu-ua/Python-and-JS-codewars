8ef4c2b0bea0375771a8d174af098c435b1fa40b
Mariia Novytska <mnovickaa@gmail.com>
2023-10-12 17:33:13 +0300

    Added task26

--- Python/231010-task26.py
+++ Python/231010-task26.py
@@ -14,3 +14,15 @@ def shortest_steps_to_num(num):
             num -= 1
         counter += 1
     return counter
+
+# ------------------------ Mariia ------------------------
+def shortest_steps_to_num(num):
+    steps = 0
+    while num > 1:
+        if num%2 == 0:
+            num = num/2
+            steps += 1
+        else:
+            num -= 1
+            steps +=1
+    return steps
