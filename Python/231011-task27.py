152532612960a62b97836e9ca47d8f8c7a0b62d6
Mariia Novytska <mnovickaa@gmail.com>
2023-10-12 17:18:09 +0300

    Added task27

--- Python/231011-task27.py
+++ Python/231011-task27.py
@@ -1,2 +1,21 @@
 #Numerical Palindrome #4
 #https://www.codewars.com/kata/58df8b4d010a9456140000c7
+
+# ------------------------ Mariia ------------------------
+def is_palindrome(n):
+    return str(n) == str(n)[::-1]
+
+def palindrome(num):
+    if not isinstance(num, int) or num < 0:
+        return "Not valid"
+    
+    if num < 15:
+        return 11
+    
+    lower_palindrome = num
+    higher_palindrome = num
+    while not (is_palindrome(lower_palindrome) or is_palindrome(higher_palindrome)):
+        lower_palindrome -= 1
+        higher_palindrome += 1
+    
+    return higher_palindrome if is_palindrome(higher_palindrome) else lower_palindrome
