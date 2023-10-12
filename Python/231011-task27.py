2e3fad014421cb681375a89eeede6935e271cb31
Olesia Barannik <OlesyaBarannik23@gmail.com>
2023-10-12 20:29:01 +0300

    added solution task27

--- Python/231011-task27.py
+++ Python/231011-task27.py
@@ -20,3 +20,34 @@ def palindrome(num):
         higher_palindrome += 1
     
     return higher_palindrome if is_palindrome(higher_palindrome) else lower_palindrome
+
+# ------------------------ Olesia ------------------------
+
+def palindrome(num):
+    if type(num) != int:
+        return "Not valid"
+    if num < 0:
+        return "Not valid"
+    if len(str(num)) == 1:
+        return 11
+
+    small_palindrome = 0
+    big_palindrome = 0
+
+    num1 = num
+
+    while str(num1) != small_palindrome:
+        num1 = num1 - 1
+        small_palindrome = str(num1)[::-1]
+
+    while str(num1) != big_palindrome:
+        num1 = num1 + 1
+        big_palindrome = str(num1)[::-1]
+
+    diff1 = abs(num - int(small_palindrome))
+    diff2 = abs(num - int(big_palindrome))
+
+    if diff1 < diff2:
+        return int(small_palindrome)
+    else:
+        return int(big_palindrome)
