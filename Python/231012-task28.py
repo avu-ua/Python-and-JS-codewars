7c2bfb7f617cc13ec0007a6fc74376065236c644
Mariia Novytska <mnovickaa@gmail.com>
2023-10-12 19:26:08 +0300

    Added task28

--- Python/231012-task28.py
+++ Python/231012-task28.py
@@ -1,2 +1,6 @@
 # A Letter's Best Friend
 # https://www.codewars.com/kata/64fc03a318692c1333ebc04c
+
+# ------------------------ Mariia ------------------------
+def best_friend(txt, a, b):
+    return True if txt.count(a+b) == txt.count(a) else False
