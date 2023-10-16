dd85068f0a61b0423c681b3e8aa3f0668755ac2b
Olesia Barannik <OlesyaBarannik23@gmail.com>
2023-10-16 17:01:11 +0300

    added solution task29

--- Python/231016-task29.py
+++ Python/231016-task29.py
@@ -1,2 +1,12 @@
 #Sums of Parts
 #https://www.codewars.com/kata/5ce399e0047a45001c853c2b
+
+# ------------------------ Olesia ------------------------
+def parts_sums(ls):
+    total_sum = sum(ls)
+    my_sum = [total_sum]
+
+    for i in ls:
+        total_sum -= i
+        my_sum.append(total_sum)
+    return my_sum
