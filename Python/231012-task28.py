5b8c721fd706f91462e043994a91f6ea6c61e30c
Olesia Barannik <OlesyaBarannik23@gmail.com>
2023-10-17 00:03:45 +0300

    added solution task28

--- Python/231012-task28.py
+++ Python/231012-task28.py
@@ -4,3 +4,11 @@
 # ------------------------ Mariia ------------------------
 def best_friend(txt, a, b):
     return True if txt.count(a+b) == txt.count(a) else False
+
+# ------------------------ Olesia ------------------------
+def best_friend(txt, a, b):
+    ab = a + b
+    for i in range(len(txt)):
+        if (txt[i] == a and (i+1 >= len(txt) or txt[i+1] != b)):
+            return False
+    return True
