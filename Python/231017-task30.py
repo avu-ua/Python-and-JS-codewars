860ae3672448a4b4fb0bedcb2e97a0f471b5a4b8
Olesia Barannik <OlesyaBarannik23@gmail.com>
2023-10-20 12:25:08 +0300

    added solution task30

--- Python/231017-task30.py
+++ Python/231017-task30.py
@@ -4,3 +4,18 @@
 # ------------------------ Mariia ------------------------
 def neutralise(s1, s2):
     return "".join([s1[x] if s1[x]==s2[x] else "0" for x in range(0, len(s1))])
+
+# ------------------------ Olesia ------------------------
+def neutralise(s1, s2):
+    result = ""
+    n = 0
+    for a in s1:
+        print(a, s2[n])
+        if a == "+" and s2[n] == "+":
+            result += "+"
+        elif a != s2[n]:
+            result += "0"
+        else:
+            result += "-"
+        n += 1
+    return result
