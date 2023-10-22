c82cf512dc1bcc4bf6dbb21cfd44f966d697590d
Mariia Novytska <mnovickaa@gmail.com>
2023-10-22 23:45:05 +0300

    Added task32

--- Python/231019-task32.py
+++ Python/231019-task32.py
@@ -21,3 +21,27 @@ def who_is_online(friends):
     if len(away) >= 1:
         result["away"] = away
     return result
+
+# ------------------------ Mariia ------------------------
+def who_is_online(friends):
+    names_dict = {
+        "online": [],
+        "offline": [],
+        "away": []
+    }
+    for x in friends:
+        if x["status"] == 'online' and x["last_activity"] <= 10:
+            names_dict["online"].append(x["username"])
+        elif x["status"] == 'online' and x["last_activity"] > 10:
+            names_dict["away"].append(x["username"])
+        else:
+            names_dict["offline"].append(x["username"])
+            
+    if names_dict["online"] == []:
+        names_dict.pop("online")
+    if names_dict["offline"] == []:
+        names_dict.pop("offline")
+    if names_dict["away"] == []:
+        names_dict.pop("away")
+            
+    return names_dict
