b9b5d8fa29351f564c6b11b582a86a949affc83e
Olesia Barannik <OlesyaBarannik23@gmail.com>
2023-10-24 19:40:12 +0300

    added solution task33

--- Python/231020-task33-34.py
+++ Python/231020-task33-34.py
@@ -1,6 +1,23 @@
 #Naughty or Nice
 #https://www.codewars.com/kata/5662b14e0a1fb8320a00005c/train/python
 
+# ------------------------ Olesia ------------------------
+def naughty_or_nice(data):
+    my_dict = {"Nice": 0, "Naughty": 0}
+    for a in data:
+        for b in data[a]:
+            if data[a][b] == "Nice":
+                my_dict["Nice"] += 1
+            else:
+                my_dict["Naughty"] += 1
+
+    if my_dict["Nice"] > my_dict["Naughty"]:
+        return "Nice!"
+    elif my_dict["Nice"] < my_dict["Naughty"]:
+        return "Naughty!"
+    else:
+        return "Nice!"
+
 
 #Count the Digit
 # https://www.codewars.com/kata/566fc12495810954b1000030/python
