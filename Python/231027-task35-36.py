b536956cf3247f0f21cc5a1e7bade31e2ba8aa84
Mariia Novytska <mnovickaa@gmail.com>
2023-10-29 11:56:21 +0200

    Added tasks 35

--- Python/231027-task35-36.py
+++ Python/231027-task35-36.py
@@ -1,6 +1,17 @@
 # Refactored Greeting
 # https://www.codewars.com/kata/5121303128ef4b495f000001
 
+# ------------------------ Mariia ------------------------
+class Person():
+    
+    def __init__ (self, name):
+        self.name = name
+
+
+    def greet(self, your_name):
+        return f"Hello {your_name}, my name is {self.name}"
 
 # Who has the most money?
 # https://www.codewars.com/kata/528d36d7cc451cd7e4000339/train/python
+
+# ------------------------ Mariia ------------------------
