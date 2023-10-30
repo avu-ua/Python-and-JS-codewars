ec7664cdf4ce3f8fccf9ae44deb4672addb1ec84
Mariia Novytska <mnovickaa@gmail.com>
2023-10-30 12:47:53 +0200

    added task 36, and task for today

--- Python/231027-task35-36.py
+++ Python/231027-task35-36.py
@@ -15,3 +15,12 @@ class Person():
 # https://www.codewars.com/kata/528d36d7cc451cd7e4000339/train/python
 
 # ------------------------ Mariia ------------------------
+def most_money(students):
+    if len(students) == 1:
+        return students[0].name
+    else:
+        name = ""
+        money_list = [studend.fives * 5 + studend.tens * 10 + studend.twenties * 20 for studend in students]
+        if len(set(money_list)) == 1:
+            return "all"
+        return students[money_list.index(max(money_list))].name
---
+++ Python/231030-task37.py
@@ -0,0 +1,2 @@
+# Merging sorted integer arrays (without duplicates)
+# https://www.codewars.com/kata/573f5c61e7752709df0005d2
