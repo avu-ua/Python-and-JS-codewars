2bb3215881fbbe2ce774418dcfdf7ad8a0112674
Mariia Novytska <mnovickaa@gmail.com>
2023-11-03 16:37:31 +0200

    Added tasks 37-39

--- Python/231030-task37-39.py
+++ Python/231030-task37-39.py
@@ -18,10 +18,38 @@ def merge_arrays(first, second):
     return my_list
 print(merge_arrays([2, 4, 8], [2, 4, 6]))
 
+# ------------------------ Mariia ------------------------
+def merge_arrays(first, second): 
+    return sorted(list(set(first+second)))
+
 #task38
 # Count by X
 #https://www.codewars.com/kata/5513795bd3fafb56c200049e/python
 
+# ------------------------ Mariia ------------------------
+def count_by(x,n):
+    lst = []
+    for y in range(1,n+1):
+        lst.append(x*y)
+    return lst
+
 #task39
 #altERnaTIng cAsE <=> ALTerNAtiNG CaSe
 #https://www.codewars.com/kata/56efc695740d30f963000557/python
+
+# ------------------------ Mariia ------------------------
+def to_alternating_case(string):
+    st = ""
+    for x in string:
+        if x.isalpha():
+            if x.isupper():
+                st += x.lower()
+            else:
+                st += x.upper()
+        else:
+            st += x
+    return st
+
+# Потім я дізналась про існування функції swapcase() :)
+def to_alternating_case(string):
+    return string.swapcase()
