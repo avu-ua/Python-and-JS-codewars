205ee2127771a1bb105f785d2dd6d97da68485f5
Olesia Barannik <OlesyaBarannik23@gmail.com>
2023-11-13 11:35:12 +0200

    added task40-41

---
+++ Python/231113-task40-41.py
@@ -0,0 +1,6 @@
+#Python's Dynamic Classes #2
+ #https://www.codewars.com/kata/55ddcef532f8678af1000006/train/python
+
+
+#Dynamic Class Creation (retired)
+#https://www.codewars.com/kata/57fab8a32b531416560005a5
